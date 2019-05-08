6e36f6b41d43d5498c85ff535e887d018e8d032e
nick.loquet <nick_loquet@live.be>
2019-05-08 12:10:42 +0200

    thymeleaf

--- src/main/java/be/vdab/luigi/controllers/IndexController.java
+++ src/main/java/be/vdab/luigi/controllers/IndexController.java
@@ -1,25 +1,29 @@
 package be.vdab.luigi.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.time.LocalTime;
 
-@RestController                         //class dat controlller is
+@Controller                             //enkel Controller bij samenwerking thymeleaf
 @RequestMapping("/")                    //associatie controller met URL
 class IndexController {
-    @GetMapping                         //verwerking GET requests, bij GET request oproep URL, voert method uit
-    public String index(){
-        StringBuffer buffer = new StringBuffer("<!doctype html><html><title>Hallo</title><body>");
+//    @GetMapping                         //verwerking GET requests, bij GET request oproep URL, voert method uit
+//    public String index(){
+//        return "index";
+//    }
+    @GetMapping
+    public ModelAndView index(){
         int uur = LocalTime.now().getHour();
         if(uur < 12){
-            buffer.append("Goede morgen");
-        }else if(uur < 18){
-            buffer.append("Goede middag");
-        }else{
-            buffer.append("Goede avond");
+            return new ModelAndView("index", "boodschap", "morgen");
         }
-        buffer.append("</body></html>");
-        return buffer.toString();
+        if(uur < 18){
+            return new ModelAndView("index", "boodschap", "middag");
+        }
+        return new ModelAndView("index", "boodschap", "avond");
     }
 }
